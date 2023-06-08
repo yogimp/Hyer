b75fd1ef8c91048c16e809018cdc5fccd87fb544
yogimp <yogimeizalputra@gmail.com>
2023-06-08 09:25:27 +0700

    Adding Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} className={styles.img} alt='hero image'/>
+      </div>
     </div>
   )
 }
