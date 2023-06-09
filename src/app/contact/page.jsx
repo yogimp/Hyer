c88f9e80726671cb0a558ef8ee3b30da3e6a67bf
yogimp <yogimeizalputra@gmail.com>
2023-06-09 13:52:43 +0700

    Adding Contact Page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
+import Button from '@/components/button/Button'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 function Contact() {
     return (
-        <div className={styles.contact}>Contact</div>
+        <div className={styles.contact}>
+            <h1 className={styles.title}>Let's Keep in Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image
+                        src="/contact.png"
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+                <form className={styles.form}>
+                    <input type="text" placeholder="name" className={styles.input} />
+                    <input type="text" placeholder="email" className={styles.input} />
+                    <textarea
+                        className={styles.textArea}
+                        placeholder="message"
+                        cols="30"
+                        rows="10"
+                    ></textarea>
+                    <Button url="#" text="Send "/>
+                </form>
+            </div>
+        </div>
     )
 }
 
