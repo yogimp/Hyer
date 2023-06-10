a243a1d75478ca5eecf513d2af3f641f7c22580e
yogimp <yogimeizalputra@gmail.com>
2023-06-10 08:28:28 +0700

    Adding Portfolio and Blog Pages

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,46 @@
+import Image from "next/image"
+import styles from './page.module.css'
 
 const BlogPost = () => {
     return (
         <div>
-            BlogPost
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <div className={styles.info}>
+                    <h1 className={styles.title}>Judul</h1>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur voluptates, adipisci culpa iure reiciendis aliquam rem consequuntur iste alias vitae. Excepturi similique deserunt omnis culpa impedit magni. Expedita, eaque praesentium.
+                    </p>
+                    <div className={styles.author}>
+                        <Image
+                            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>User Name</span>
+                    </div>
+                    </div>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src= "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+                            alt=""
+                            fill={true}
+                            className={styles.image}
+                        />
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
+                    </p>
+                    <p className={styles.text}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 import styles from './page.module.css'
 
 const Portfolio = () => {
     return (
         <div className={styles.container}>
-            Portfolio
+            <h1 className={styles.selectTitle}>Choose a gallery</h1>
+            <div className={styles.items}>
+                <Link href="/portfolio/illustrations" className={styles.item}>
+                    <span className={styles.title}>Illustrations</span>
+                </Link>
+                <Link href="/portfolio/websites" className={styles.item}>
+                    <span className={styles.title}>Websites</span>
+                </Link>
+                <Link href="/portfolio/application" className={styles.item}>
+                    <span className={styles.title}>Application</span>
+                </Link>
+            </div>
         </div>
     )
 }
