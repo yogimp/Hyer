81460a8fbf5c491cb3db395ce2c523f78f5879fc
yogimp <yogimeizalputra@gmail.com>
2023-06-13 16:28:14 +0700

    Geta data from local JSON file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,27 +1,43 @@
 import Button from '@/components/button/Button'
 import styles from './page.module.css'
 import Image from 'next/image'
+import {items} from './data.js'
+import { notFound } from 'next/navigation'
+
+const getData = (cat) => {
+    const data = items[cat]
+
+    if(data) {
+        return data
+    }
+
+    return notFound();
+}
 
 const Category = ({params}) => {
-    console.log(params)
+    const data = getData(params.category)
+
     return (
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.category}</h1>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Judul</h1>
-                    <p className={styles.desc}>Desc</p>
-                    <Button text="See More" url="#" />
+            {data.map(item => (
+                <div className={styles.item} key={item.id}>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.desc}>{item.desc}</p>
+                        <Button text="See More" url="#" />
+                    </div>
+                    <div className={styles.imgContainer}>
+                        <Image
+                            className={styles.img}
+                            fill={true}
+                            src={item.image}
+                            alt=""
+                        />
+                    </div>
                 </div>
-                <div className={styles.imgContainer}>
-                    <Image
-                        className={styles.img}
-                        fill={true}
-                        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
-                        alt=""
-                    />
-                </div>
-            </div>
+            ))}
+            
         </div>
     )
 }
