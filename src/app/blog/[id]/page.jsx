a383637a8f0074ca994e6f6a016e458a947e6ce6
yogimp <yogimeizalputra@gmail.com>
2023-06-13 11:29:46 +0700

    Fetching data Server Side and Client Side(SWR)

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,30 +1,44 @@
-import Image from "next/image"
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const BlogPost = () => {
-    return (
-        <div>
+async function getData(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        cache: "no-store",
+    });
+
+    if (!res.ok) {
+        return notFound()
+    }
+
+    return res.json();
+}
+
+    const BlogPost = async ({ params }) => {
+        const data = await getData(params.id);
+
+        return (
             <div className={styles.container}>
                 <div className={styles.top}>
                     <div className={styles.info}>
-                    <h1 className={styles.title}>Judul</h1>
-                    <p className={styles.desc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur voluptates, adipisci culpa iure reiciendis aliquam rem consequuntur iste alias vitae. Excepturi similique deserunt omnis culpa impedit magni. Expedita, eaque praesentium.
-                    </p>
-                    <div className={styles.author}>
-                        <Image
-                            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
-                            alt=""
-                            width={40}
-                            height={40}
-                            className={styles.avatar}
-                        />
-                        <span className={styles.username}>User Name</span>
-                    </div>
+                        <h1 className={styles.title}>{data.title}</h1>
+                        <p className={styles.desc}>
+                            {data.body}
+                        </p>
+                        <div className={styles.author}>
+                            <Image
+                                src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+                                alt=""
+                                width={40}
+                                height={40}
+                                className={styles.avatar}
+                            />
+                            <span className={styles.username}>username</span>
+                        </div>
                     </div>
                     <div className={styles.imageContainer}>
                         <Image
-                            src= "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+                            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
                             alt=""
                             fill={true}
                             className={styles.image}
@@ -33,16 +47,14 @@ const BlogPost = () => {
                 </div>
                 <div className={styles.content}>
                     <p className={styles.text}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem nesciunt recusandae amet porro, temporibus excepturi, magni unde voluptatem rem veniam laborum, soluta maiores accusantium molestiae! Assumenda quos labore minus?
                     </p>
                     <p className={styles.text}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, provident fuga inventore rem quod vero voluptates harum illo tenetur molestias!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium exercitationem nesciunt recusandae amet porro, temporibus excepturi, magni unde voluptatem rem veniam laborum, soluta maiores accusantium molestiae! Assumenda quos labore minus?
                     </p>
                 </div>
             </div>
-        </div>
-    )
-}
+        );
+    };
 
-export default BlogPost
+export default BlogPost;
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,44 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import useSWR from 'swr'
 
 const Dashboard = () => {
+
+    //THIS FETCH DATA USING USEEFFECT
+
+    // const [data, setData] = useState([]);
+    // const [err, setErr] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         setIsLoading(true);
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    //             cache: "no-store",
+    //         });
+
+    //         if (!res.ok) {
+    //             setErr(true);
+    //         }
+
+    //         const data = await res.json()
+
+    //         setData(data);
+    //         setIsLoading(false);
+    //     };
+    //     getData()
+    // }, []);
+
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());      
+
+    const { data, error, isLoading } = useSWR(
+        "https://jsonplaceholder.typicode.com/posts",
+        fetcher
+    );
+    console.log(data)
+
     return (
         <div className={styles.container}>
             Dashboard
